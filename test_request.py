ec873806f54ae4c155b629b72c5951a99dca47a9
pablocalderon-sys <pablocaldern4@gmail.com>
2026-06-27 21:46:50 +0200

    test_request actualizado

--- test_request.py
+++ test_request.py
@@ -1,9 +1,22 @@
 import requests
 from datetime import datetime, date
 
-
-
 url = "https://127.0.0.1:8000/tasks/"
+
+def exec_post_request(user_data, url, expected_code):
+
+    response = requests.post(url, json=user_data, timeout=5)
+
+    print(f"Código de respuesta: {response.status_code}")
+    print(f"Respuesta: {response.json()}")
+    assert response.status_code == expected_code
+   
+    if response.status_code == 200:
+        print(f"Respuesta: {response.json()}")
+    elif response.status_code != 500:
+        print(f"Respuesta: {response.json()}")
+    else:
+        print("Internal Server Error")
 
 datos_tarea = {
     "titulo": "Compra Supermercado",
@@ -11,6 +24,4 @@ datos_tarea = {
     "Creada": date(2026,3,1).isoformat()
 }
 
-respuesta = requests.post(url,datos_tarea)
-print(f"Codigo de respuesta: {respuesta.status_code}")
-print(f"Respuesta:{respuesta.json}")
+exec_post_request(datos_tarea,url,200)
